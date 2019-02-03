1eced38743d41aa617bc8b299906dbaeeb8702d9
Christopher Easton <ceaston7@hotmail.com>
2019-02-02 20:07:26 -0500

    Init

---
+++ Assets/Scripts/Scaler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OurGame
+{
+		public class Scaler : MonoBehaviour
+		{
+				Vector3 scaleVector = new Vector3(1,1,1);
+				public float scaleFactor = 0.2f;
+				public void scale(float growOrShrink)
+				{
+						gameObject.transform.localScale += (scaleVector * scaleFactor * gameObject.transform.localScale.x * growOrShrink * Time.deltaTime);
+				}
+		}
+}
