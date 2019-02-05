74bc1f8ac7f61ff90caa4cce74d9a77f13a92eba
alanaer <ericksonalanal@gmail.com>
2019-02-05 13:06:16 -0500

    Update PickUp.cs

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
+using UnityStandardAssets.CrossPlatformInput;
 using UnityEngine;
 
 namespace OurGame
@@ -7,33 +8,20 @@ namespace OurGame
     public class PickUp : MonoBehaviour
     {
         public Transform pickUpObject;
-        public GameObject item;
-        public GameObject temp;
-
-        private void Start()
-        {
-            item.GetComponent<Rigidbody>().useGravity = true;
-        }
-
-        private void Update()
-        {
-        }
 
         void OnMouseDown()
         {
-            item.GetComponent<Rigidbody>().useGravity = false;
-            item.GetComponent<Rigidbody>().isKinematic = true;
-            item.transform.position = pickUpObject.position;
-            item.transform.rotation = pickUpObject.rotation;
-            item.transform.parent = temp.transform;
+            GetComponent<Rigidbody>().useGravity = false;
+            GetComponent<Rigidbody>().isKinematic = true;
+            this.transform.position = pickUpObject.position;
+            this.transform.parent = GameObject.Find("FirstPersonCharacter").transform;
         }
 
         void OnMouseUp()
         {
-            item.transform.parent = null;
-            item.transform.position = pickUpObject.transform.position;
-            item.GetComponent<Rigidbody>().useGravity = true;
-            item.GetComponent<Rigidbody>().isKinematic = false;
+            this.transform.parent = null;
+            GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<Rigidbody>().isKinematic = false;
         }
 
     }
