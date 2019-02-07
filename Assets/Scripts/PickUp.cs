07983ee06272eff93af23d1d61291b735c26e825
alanaer <ericksonalanal@gmail.com>
2019-02-07 00:53:30 -0500

    Only pick up objects tagged "object"

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -6,40 +6,41 @@ namespace OurGame
 
     /* 
      * To use:
-     * Select playercharacter, create child object "PickUp"
+     * Select playercharacter, create child object "Hands"
      * Add script to Cube
-     * Pick Up = drag child object "PickUp"
+     * Hold = drag child object "Hands"
      * Cam = drag playercharacter camera
+     * Tag Cube as "object"
      * 
      * Key code is Fire3, we are setting it to 'e'
      */
 
     public class PickUp : MonoBehaviour
     {
-        public Transform pickUp;
-        public Camera cam;
+        public Transform Hold;
+        public Camera Cam;
         private bool holding;
 
         private void Start()
         {
-            cam = Camera.main;
+            Cam = Camera.main;
             GetComponent<Rigidbody>().useGravity = true;
         }
 
         private void Update()
         {
             RaycastHit hit;
-            Ray ray = cam.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+            Ray ray = Cam.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
 
             if (CrossPlatformInputManager.GetButtonDown("Fire3"))
             {
                 //If you're not holding the object, pick it up with Fire3
-                if (!holding && Physics.Raycast(ray, out hit, 3.0f))
+                if (!holding && Physics.Raycast(ray, out hit, 3.0f) && hit.collider.tag == "object")
                 {
                     GetComponent<Rigidbody>().useGravity = false;
                     GetComponent<Rigidbody>().isKinematic = true;
-                    this.transform.position = pickUp.position;
-                    this.transform.parent = GameObject.Find("FirstPersonCharacter").transform;
+                    this.transform.position = Hold.position;
+                    this.transform.parent = GameObject.FindWithTag("player").transform;
                     holding = true;
                 }
                 //If you are holding the object, drop it with Fire3
