3098fb94ff1d10190737fb1b069cf60426f1dccd
alanaer <ericksonalanal@gmail.com>
2019-03-10 23:34:00 -0400

    script now creates a hand and deletes it on drop

--- Assets/Scripts/Controls/PickUp.cs
+++ Assets/Scripts/Controls/PickUp.cs
@@ -5,58 +5,78 @@ namespace OurGame
 {
     public class PickUp : MonoBehaviour
     {
-        private GameObject obj;
         private bool held;
-
-        private void Start()
-        {
-            obj = transform.gameObject;
-        }
+        private GameObject hand;
 
         private void Update()
         {
-            if(obj.GetComponent<FixedJoint>() == null && held)
+            // If the object collides with something and breaks the joint
+            // update the status of it being held and destroy hand.
+            if(gameObject.GetComponent<FixedJoint>() == null && held)
             {
-                held = false;
+                Destroy(hand);
+                transform.gameObject.transform.parent = null;
+                StartCoroutine(UpdateHolding(false));
             }
         }
 
-        public void Move(PlayerControllerScale control)
+        // Helper method to either pick up or drop object
+        public void Move()
         {
             if (!held)
             {
-                Pick(control.hand);
+                Pick();
             }
             else
             {
                 Drop();
             }
-
-            StartCoroutine(UpdateHolding());
         }
 
-        private void Pick(GameObject hand)
+        private void Pick()
         {
-            obj.transform.position = hand.transform.position;
-            obj.transform.localRotation = Camera.main.transform.rotation;
+            if (hand == null)
+            {
+                // Create a hand object and place it infront of the player
+                hand = new GameObject("Hand");
+                hand.transform.parent = Camera.main.transform;
+                GameObject player = Camera.main.transform.parent.gameObject;
+                hand.transform.position = player.transform.position + player.transform.forward * 2.5f + player.transform.up * 0.2f;
+
+                Rigidbody body = hand.AddComponent<Rigidbody>();
+                body.isKinematic = true;
+                body.useGravity = false;
+            }
+
+            // Move object into hand
+            gameObject.transform.position = hand.transform.position;
+            gameObject.transform.localRotation = Camera.main.transform.rotation;
+            gameObject.transform.SetParent(hand.transform);
  
-            FixedJoint joint = obj.gameObject.AddComponent<FixedJoint>();
+            // Give object a FixedJoint to maintain physics
+            // Break force for wall collisions. Sensitivity can be edited (or removed entirely).
+            FixedJoint joint = transform.gameObject.AddComponent<FixedJoint>();
             joint.connectedBody = hand.GetComponent<Rigidbody>();
             joint.breakForce = 10000f;
+
+            StartCoroutine(UpdateHolding(true));
         }
 
         private void Drop()
         {
-            Destroy(obj.GetComponent<FixedJoint>());
-            obj.transform.parent = null;
-            obj.transform.localRotation = transform.rotation;
+            //Remove object from hand and destroy the joint/hand
+            gameObject.transform.parent = null;
+            Destroy(transform.gameObject.GetComponent<FixedJoint>());
+            Destroy(hand);
+
+            StartCoroutine(UpdateHolding(false));
 
         }
 
-        IEnumerator UpdateHolding()
+        IEnumerator UpdateHolding(bool status)
         {
-            held = !held;
-            yield return new WaitForSeconds(0.2f);
+            yield return new WaitForSeconds(0.5f);
+            held = status;
         }
     }
 }
