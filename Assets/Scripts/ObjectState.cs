bef3e22a3812450b0c59359306ebc0d8a4e39ef8
alanaer <ericksonalanal@gmail.com>
2019-03-19 12:52:15 -0400

    Track object state

---
+++ Assets/Scripts/ObjectState.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectState : MonoBehaviour
+{
+    private Vector3 StartLoc;
+    private Vector3 StartSize;
+
+    private void Start()
+    {
+        StartLoc = transform.position;
+        StartSize = GetComponent<Renderer>().bounds.size;
+    }
+
+    public Vector3 getStartLocation()
+    {
+        return StartLoc;
+    }
+
+    public Vector3 getStartSize()
+    {
+        return StartSize;
+    }
+}
