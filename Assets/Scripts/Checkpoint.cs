2a63addc2d7c2a86c37ac3cdffd012f2c0fbb142
alanaer <ericksonalanal@gmail.com>
2019-04-09 09:39:14 -0400

    Checkpoint, LevelState, and KillFloor

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OurGame
+{
+    public class Checkpoint : MonoBehaviour
+    {
+        private GameObject levelState;
+
+        private void Start()
+        {
+            levelState = GameObject.Find("LevelState");
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.CompareTag("Player"))
+            {
+                levelState.GetComponent<LevelState>().Checkpoint();
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/LevelState.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OurGame
+{
+    public class LevelState : MonoBehaviour
+    {
+        private GameObject player;
+        
+        private void Start()
+        {
+            player = GameObject.FindWithTag("Player");
+        }
+
+        public void Respawn()
+        {
+            Debug.Log("Begin Respawn");
+            player.transform.position = player.GetComponent<PlayerState>().spawn.position;
+
+            var objects = GameObject.FindGameObjectsWithTag("Object");
+            foreach(var item in objects)
+            {
+                item.transform.position = item.GetComponent<ObjectState>().GetSpawnLocation();
+                item.transform.localScale = item.GetComponent<ObjectState>().GetSpawnSize();
+            }
+        }
+
+        public void Checkpoint()
+        {
+            Debug.Log("Checkpoint");
+            player.GetComponent<PlayerState>().spawn.position = player.transform.position;
+
+            var objects = GameObject.FindGameObjectsWithTag("Object");
+            foreach (var item in objects)
+            {
+                item.GetComponent<ObjectState>().SetSpawn();
+            }
+        }
+    }
+}
+
