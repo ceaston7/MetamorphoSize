a0643ef1158da2853d6bd1272447cfe9d930071f
Christopher Easton <ceaston7@hotmail.com>
2019-04-15 22:51:25 -0400

    Level 1 edits, UI

---
+++ Assets/Scripts/LevelChange.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChange : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other){
+				if(other.tag == "Player"){
+						SceneManager.LoadScene("Level2");
+				}
+		}
+}
